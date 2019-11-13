7f005414aae8c782dd5f26501c9669e5c437e166
hudark076 <57371855+hudark076@users.noreply.github.com>
2019-11-13 00:17:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] intarray = new int[3] {10, 20,30};
+            //if the lenth of the array is not known ARRAYINT.LENGHT can be used.
+            for (int i = 0; i < intarray.Length;i++)
+            // for (int =0; i<=2; i++)
+            {
+                Console.WriteLine(intarray[i]);
+            }
+            Console.ReadLine();
+            //fix this*/
+
+        string[] namen = new string[4] { "mik", "bik", "vik", "cik" };
+            for (int i=0; i<=4; i++)
+            {
+                Console.WriteLine(namen[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}

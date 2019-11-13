e2a37b06b93b528b9333fd3f13efb17f9f3195ca
hudark076 <57371855+hudark076@users.noreply.github.com>
2019-11-13 00:20:38 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //PRINT HELLO WORLD//
+            Console.WriteLine("!!!Hello world!!!");
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
